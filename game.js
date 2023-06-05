79d051d689e1076c290728a4d9dfc7fa056bb317
neslihan <neslihan_atasvr111@outlook.com>
2023-06-05 07:51:04 +0300

    create and style the end page

--- game.js
+++ game.js
@@ -57,6 +57,7 @@ startGame = () => {
 
 getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
     return window.location.assign("/end.html");
     //go to the end page
   }
